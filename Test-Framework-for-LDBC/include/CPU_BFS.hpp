6b6139e8823188d4badd20160208ac70fb767c47
toziki <1309975419@qq.com>
2024-05-25 16:58:55 +0800

    add  annotation

--- Test-Framework-for-LDBC/include/CPU_BFS.hpp
+++ Test-Framework-for-LDBC/include/CPU_BFS.hpp
@@ -10,7 +10,7 @@ std::vector<int> CPU_BFS(std::vector<std::vector<std::pair<int, T>>>& input_grap
 
 	std::vector<int> depth(N, INT_MAX);
 	depth[root] = 0;
-
+	//Prevent duplicate traversal while recording depth
 	std::vector<int> searched_vertices;
 
 	std::queue<int> Q; // Queue is a data structure designed to operate in FIFO (First in First out) context.
@@ -23,8 +23,10 @@ std::vector<int> CPU_BFS(std::vector<std::vector<std::pair<int, T>>>& input_grap
 		Q.pop(); //Removing that vertex from queue,whose neighbour will be visited now
 
 		if (depth[v] + 1 <= max_depth) {
+			//Traversing node v in the graph yields a pair value, adjfirst being the adjacency point
 			for (auto& adj : input_graph[v]) { /*processing all the neighbours of v*/
 				if (depth[adj.first] > depth[v] + 1) {
+					//If the depth of adjacent points is greater, add them to the queue. Otherwise, it means that the adjacent points have already been traversed before
 					depth[adj.first] = depth[v] + 1;
 					Q.push(adj.first);
 				}
