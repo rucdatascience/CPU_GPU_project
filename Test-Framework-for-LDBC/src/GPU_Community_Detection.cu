c4b0052646710a400862a63aadb80f65c5411516
toziki <1309975419@qq.com>
2024-05-31 23:23:02 +0800

    comment GPU CDLP

--- Test-Framework-for-LDBC/src/GPU_Community_Detection.cu
+++ Test-Framework-for-LDBC/src/GPU_Community_Detection.cu
@@ -6,7 +6,7 @@ static int CD_ITERATION;
 static int CD_SET_THREAD;
 // static int CD_M;
 static vector<int> outs_ptr, ins_ptr, outs_neighbor, ins_neighbor, in_out_ptr;
-
+//The pointer indicates the starting and ending positions of the vertex's outgoing and incoming edges
 static int *in_out_ptr_gpu;
 static int *outs_ptr_gpu, *ins_ptr_gpu;
 static int *outs_neighbor_gpu, *ins_neighbor_gpu;
@@ -16,19 +16,19 @@ static int *global_space_for_label;
 template <typename T>
 void pre_set(graph_structure<T> &graph, int &CD_GRAPHSIZE)
 {
-
+    
     CSR_graph<T> ARRAY_graph;
     ARRAY_graph = graph.toCSR();
 
     CD_ITERATION = graph.cdlp_max_its;
     CD_GRAPHSIZE = ARRAY_graph.OUTs_Neighbor_start_pointers.size() - 1;
     // CD_SET_THREAD = 100;
-
+    //Preprocessing range pointer
     outs_ptr.resize(CD_GRAPHSIZE + 1);
     outs_ptr = ARRAY_graph.OUTs_Neighbor_start_pointers;
     ins_ptr.resize(CD_GRAPHSIZE + 1);
     ins_ptr = ARRAY_graph.INs_Neighbor_start_pointers;
-    in_out_ptr.resize(CD_GRAPHSIZE + 1);
+    in_out_ptr.resize(CD_GRAPHSIZE + 1);//for all Adjacency edge
     in_out_ptr[0] = 0;
     for (int i = 1; i <= CD_GRAPHSIZE; ++i)
     {
@@ -37,7 +37,7 @@ void pre_set(graph_structure<T> &graph, int &CD_GRAPHSIZE)
     // for(int i=0;i<10000;++i){
     //     cout<<outs_ptr[i]<<"  "<<ins_ptr[i]<<"  "<<in_out_ptr[i]<<endl;
     // }
-    outs_neighbor = ARRAY_graph.OUTs_Edges;
+    outs_neighbor = ARRAY_graph.OUTs_Edges;//copy data
     ins_neighbor = ARRAY_graph.INs_Edges;
 
     // int sum = 0;
@@ -63,6 +63,7 @@ void pre_set(graph_structure<T> &graph, int &CD_GRAPHSIZE)
 
 __global__ void init_label(int *labels_gpu,int *new_labels_gpu, int CD_GRAPHSIZE)
 {
+    //Initialize all labels at once with GPU.Initially, each vertex v is assigned a unique label which matches its identifier.
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid >= 0 && tid < CD_GRAPHSIZE)
     {
@@ -72,17 +73,18 @@ __global__ void init_label(int *labels_gpu,int *new_labels_gpu, int CD_GRAPHSIZE
 }
 
 __global__ void extract_labels(int *in_out_ptr_gpu, int *ins_ptr_gpu, int *outs_ptr_gpu, int *ins_neighbor_gpu, int *outs_neighbor_gpu, int *labels, int *labels_out, int CD_GRAPHSIZE)
-{
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+{   //This function is used to obtain the labels of vertex neighbors
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;//Calculate which vertex to process
     if (tid >= CD_GRAPHSIZE)
         return;
 
-    int start = in_out_ptr_gpu[tid];
-    int len_out = outs_ptr_gpu[tid + 1] - outs_ptr_gpu[tid];
-    int len_in = ins_ptr_gpu[tid + 1] - ins_ptr_gpu[tid];
+    int start = in_out_ptr_gpu[tid];//start pointer of all edges
+    int len_out = outs_ptr_gpu[tid + 1] - outs_ptr_gpu[tid];//length of out edges
+    int len_in = ins_ptr_gpu[tid + 1] - ins_ptr_gpu[tid];//length of in edges
     // int end = in_out_ptr_gpu[tid + 1];
     for (int i = 0; i < len_in; ++i)
     {
+        //get neighbor's location->get neighbor->get label
         labels_out[start + i] = labels[ins_neighbor_gpu[ins_ptr_gpu[tid] + i]];
     }
     
@@ -90,7 +92,7 @@ __global__ void extract_labels(int *in_out_ptr_gpu, int *ins_ptr_gpu, int *outs_
     {
         labels_out[start + len_in + i] = labels[outs_neighbor_gpu[outs_ptr_gpu[tid] + i]];
     }
-    labels_out[start + len_in+len_out ] = labels[tid];
+    labels_out[start + len_in+len_out ] = labels[tid];//vertex it self 
 
     return;
 }
@@ -104,7 +106,13 @@ __global__ void parallel_sort_labels(int *in_out_ptr_gpu, int *labels_out, int C
     int start = in_out_ptr_gpu[tid];
     int end = in_out_ptr_gpu[tid + 1];
     thrust::sort(thrust::device, labels_out + start, labels_out + end);
+    //thrust::sort is a cuda function
     __syncthreads();
+    /* __Syncthreads() is a crucial synchronization primitive in CUDA programming. 
+    Its function is to ensure that all threads in a certain thread block have 
+    completed all their previous instructions before executing this function. 
+    Once all threads reach this synchronization point, 
+    they can continue executing instructions after __ syncthreads. */
 }
 
 inline void swap(int &a, int &b) {
@@ -115,6 +123,7 @@ inline void swap(int &a, int &b) {
 
 // QuickSort function for raw pointers
 void quickSort(int* arr, int low, int high) {
+    //Using Fast and Slow Pointer Method to Complete QuickSorti
     if (low < high) {
         int pivot = arr[high]; // choosing the last element as pivot
         int i = low - 1;
@@ -180,7 +189,7 @@ void get_size()
 }
 
 __global__ void LPA(int *global_space_for_label, int *in_out_ptr_gpu, int *labels_gpu, int *new_labels_gpu, int CD_GRAPHSIZE)
-{
+{   //Use GPU to propagate all labels at the same time.
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
     if (tid >= CD_GRAPHSIZE)
         return;
@@ -191,20 +200,23 @@ __global__ void LPA(int *global_space_for_label, int *in_out_ptr_gpu, int *label
     int max_count = current_count;
     for (int i = start; i < end; ++i)
     {
+        /* After the previous sorting, the labels in the array are now ordered, 
+        so the number of occurrences is calculated in order for each label */
         if (global_space_for_label[i] == current_label)
-        {
+        {   //If it is still the label from the previous calculation, continue to increase it
             current_count++;
         }
         else
         {
+            //If the label changes,update the most frequently label and start accumulating again from 1
             if (current_count > max_count)
-            {
+            {   
                 max_count = current_count;
                 max_label = current_label;
             }
             else if (current_count == max_count && current_label < max_label)
             {
-                max_label = current_label;
+                max_label = current_label;//In case there are multiple labels with the maximum frequency, the smallest label is chosen. 
             }
             current_label = global_space_for_label[i];
             current_count = 1;
@@ -217,7 +229,7 @@ __global__ void LPA(int *global_space_for_label, int *in_out_ptr_gpu, int *label
     }
     else if (current_count == max_count && current_label < max_label)
     {
-        max_label = current_label;
+        max_label = current_label;  //In case there are multiple labels with the maximum frequency, the smallest label is chosen. 
     }
     new_labels_gpu[tid] = max_label;
 }
@@ -240,7 +252,7 @@ int Community_Detection(graph_structure<double> &graph, float *elapsedTime, vect
     cudaMalloc(&ins_neighbor_gpu, ins_neighbor.size() * sizeof(int));
     cudaMallocManaged(&global_space_for_label, (outs_neighbor.size() + ins_neighbor.size() + CD_GRAPHSIZE) * sizeof(int));
     cudaMalloc(&in_out_ptr_gpu, (CD_GRAPHSIZE + 1) * sizeof(int));
-
+    //allocate cuda memory and copy data from host to device
     cudaMemcpy(outs_ptr_gpu, outs_ptr.data(), outs_ptr.size() * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(ins_ptr_gpu, ins_ptr.data(), ins_ptr.size() * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(outs_neighbor_gpu, outs_neighbor.data(), outs_neighbor.size() * sizeof(int), cudaMemcpyHostToDevice);
@@ -267,8 +279,10 @@ int Community_Detection(graph_structure<double> &graph, float *elapsedTime, vect
     // CD_ITERATION=3;
     while (it < CD_ITERATION)
     {
+        //The difference between if and else lies in the use of different old and new pointers
         if (it % 2 == 0)
         {
+            //Calculate the neighbor label array for each vertex
             extract_labels<<<init_label_block, init_label_thread>>>(in_out_ptr_gpu, ins_ptr_gpu, outs_ptr_gpu, ins_neighbor_gpu, outs_neighbor_gpu, labels_gpu, global_space_for_label, CD_GRAPHSIZE);
             err = cudaDeviceSynchronize();
             checkCudaError(err, "cudaDeviceSynchronize after extract_labels");
@@ -284,7 +298,7 @@ int Community_Detection(graph_structure<double> &graph, float *elapsedTime, vect
             // err = cudaDeviceSynchronize();
             // checkCudaError(err, "cudaDeviceSynchronize after parallel_sort_labels");
             for(int i=0;i<CD_GRAPHSIZE;++i){
-                quickSort(global_space_for_label,in_out_ptr[i],  in_out_ptr[i+1]-1);
+                quickSort(global_space_for_label,in_out_ptr[i],  in_out_ptr[i+1]-1);//Sort the array of neighbor labels for each vertex
             }
             
             // for(int i=0;i<10;++i){
@@ -294,6 +308,7 @@ int Community_Detection(graph_structure<double> &graph, float *elapsedTime, vect
             //     }
             //     cout<<endl<<"--------------------------------------------------------------"<<endl;
             // }
+            //Calculate vertex labels for this round
             LPA<<<init_label_block, init_label_thread>>>(global_space_for_label, in_out_ptr_gpu, labels_gpu, new_labels_gpu, CD_GRAPHSIZE);
             err = cudaDeviceSynchronize();
             checkCudaError(err, "cudaDeviceSynchronize after LPA");
