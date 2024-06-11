b8911d56a89a03fafdfb25be9d839f2f113f58ae
toziki <1309975419@qq.com>
2024-06-11 20:50:27 +0800

    thread

--- Test-Framework-for-LDBC/include/CPU_PageRank_update.hpp
+++ Test-Framework-for-LDBC/include/CPU_PageRank_update.hpp
@@ -2,16 +2,37 @@
 #include <graph_structure/graph_structure.hpp>
 #include <vector>
 #include <iostream>
+#include <mutex>
+#include <omp.h>
+/* to run this file,you need add below code to Cmakelists:
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fopenmp")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fopenmp")
+you must not use clock() fun in test.cpp!
+
+if (graph.sup_pr) {
+        int pr_pass = 0;
+        
+        vector<double> cpu_pr_result, gpu_pr_result;
+        cpu_pr_result = PageRank(graph.INs,graph.OUTs,graph.pr_damping,graph.pr_its);
+       
+        pr_checker_cpu(graph, cpu_pr_result, pr_pass);
+   
+    }
+
+ */
 using namespace std;
 double teleport, d;
 int GRAPHSIZE;
 std::vector<int> sink;
 
-
-std::vector<double> PageRank(std::vector<std::vector<std::pair<int, double>>> in_edge, std::vector<std::vector<std::pair<int, double>>> out_edge, double damp, int iters)
+std::vector<double> PageRank(std::vector<std::vector<std::pair<int, double>>> &in_edge, std::vector<std::vector<std::pair<int, double>>> &out_edge, double damp, int iters)
 {
-
+    double start; 
+double end; 
+start = omp_get_wtime(); 
+    
     GRAPHSIZE = in_edge.size();
+
     std::vector<double> pr(GRAPHSIZE, 1 / GRAPHSIZE);
     std::vector<double> npr(GRAPHSIZE);
     d = damp;
@@ -21,36 +42,42 @@ std::vector<double> PageRank(std::vector<std::vector<std::pair<int, double>>> in
         if (out_edge[i].size() == 0)
             sink.push_back(i);
     }
-
-    for (int i = 0; i < iters; i++)
+    
+    for (int k = 0; k < iters; k++)
     {
         double red = 0;
-        for (int i = 0; i < sink.size(); i++)
-        {
-            red += pr[sink[i]];
-        }
-        for (int i = 0; i < GRAPHSIZE; i++)
-        {
-            pr[i]/=out_edge[i].size();
-        }
+            #pragma omp parallel for shared(pr,sink) reduction(+ : red)
+            for (int i = 0; i < sink.size(); i++)
+            {
+                red += pr[sink[i]];
+            }
+            #pragma omp parallel for shared(pr,out_edge)
+            for (int i = 0; i < GRAPHSIZE; i++)
+            {
+                pr[i] /= out_edge[i].size();
+            }
         
+
         red = red * d / GRAPHSIZE;
-        std::fill(npr.begin(), npr.end(), teleport+red);
+        std::fill(npr.begin(), npr.end(), teleport + red);
+        #pragma omp parallel for shared(pr,in_edge,npr)
         for (int i = 0; i < GRAPHSIZE; i++)
-        {
+        {   
+            double temp = npr[i];
+            
             for (int j = 0; j < in_edge[i].size(); j++)
             {
-                
-                npr[i] += pr[in_edge[i][j].first];
+                temp += pr[in_edge[i][j].first];
             }
+            npr[i] = temp*d;
         }
+
         
-        for (int i = 0; i < GRAPHSIZE; i++)
-        {
-            npr[i] *= d;
-        }
+
         pr.swap(npr);
-        
     }
+end = omp_get_wtime(); 
+
+printf("CPU PageRank cost time: %f ms\n", (end - start)*1000); // 打印工作所花费的时间，即end和start的差值
     return pr;
 }
