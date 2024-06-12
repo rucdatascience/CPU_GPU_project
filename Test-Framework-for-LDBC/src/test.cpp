45de7796ed9967a06588ff85df93df1f3391ae55
CSLiuPeng <liu_peng_email@163.com>
2024-06-12 19:16:41 +0800

    update test.cpp time format

--- Test-Framework-for-LDBC/src/test.cpp
+++ Test-Framework-for-LDBC/src/test.cpp
@@ -3,12 +3,12 @@
 #include <GPU_shortest_paths.cuh>
 #include <GPU_PageRank.cuh>
 #include <GPU_Community_Detection.cuh>
-
+#include <chrono>
 #include <CPU_BFS.hpp>
 #include <CPU_connected_components.hpp>
 #include <CPU_shortest_paths.hpp>
 #include <CPU_PageRank.hpp>
-#include <CPU_PageRank_update2.hpp>
+#include <CPU_PageRank_update3.hpp>
 #include <CPU_Community_Detection.hpp>
 #include <cdlp_cpu.hpp>
 #include <checker.hpp>
@@ -26,12 +26,9 @@ void writeToCSV(const unordered_map<string, string>& data, const string& filenam
 int main()
 {
 
-    // auto begin = std::chrono::high_resolution_clock::now();
-    // auto end = std::chrono::high_resolution_clock::now();
-    // double t = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
-
     // std::string config_file = "datagen-7_5-fb.properties";//quick test
     // std::string config_file = "cit-Patents.properties";//quick test
+
     string config_file;
     std::cout << "Enter the name of the configuration file:" << std::endl;
     std::cin >> config_file;
@@ -41,47 +38,57 @@ int main()
     graph_structure<double> graph;
     //Read the ldbc configuration file to obtain key parameter information in the file
     graph.read_config(config_file);
+
+    auto begin = std::chrono::high_resolution_clock::now();
     //Read the vertex and edge files corresponding to the configuration file
     graph.load_LDBC();
     // The vertex information in graph is converted to csr format for storage
+    auto end = std::chrono::high_resolution_clock::now();
+    double load_ldbc_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+    printf("load_ldbc_time cost time: %f s\n", load_ldbc_time);
+
+    begin = std::chrono::high_resolution_clock::now();
     CSR_graph<double> csr_graph = graph.toCSR();
+    end = std::chrono::high_resolution_clock::now();
+    double graph_to_csr_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
     std::cout << "Number of vertices: " << csr_graph.OUTs_Neighbor_start_pointers.size() << std::endl;
     std::cout << "Number of edges: " << csr_graph.OUTs_Edges.size() << std::endl;
+    printf("graph_to_csr_time cost time: %f s\n", graph_to_csr_time);
+
 
     float elapsedTime = 0;
-
-    clock_t start = clock(), end = clock();
+    clock_t start = clock(), ends = clock();
 
     unordered_map<string, string> umap_all_res;
     
     size_t lastSlashPos = config_file.find_last_of("/\\");
     size_t lastDotPos = config_file.find_last_of(".");
     string test_file_name = config_file.substr(lastSlashPos + 1, lastDotPos - lastSlashPos - 1);
-    // cout<<"test_file_name:"<<test_file_name<<endl;
 
     umap_all_res.emplace("test_file_name", test_file_name);
 
     if (graph.sup_bfs) {
         int bfs_pass = 0;
         std::vector<int> cpu_bfs_result;
-        start = clock();
+        // start = clock();
+        begin = std::chrono::high_resolution_clock::now();
         cpu_bfs_result = CPU_BFS<double>(graph.OUTs, graph.bfs_src);
-        end = clock();
-        double cpu_bfs_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        printf("CPU BFS cost time: %f ms\n", cpu_bfs_time);
+        // end = clock();
+        // double cpu_bfs_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+        end = std::chrono::high_resolution_clock::now();
+        double cpu_bfs_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("CPU BFS cost time: %f s\n", cpu_bfs_time);
 
         std::vector<int> gpu_bfs_result;
-        start = clock();
+        // start = clock();
+        begin = std::chrono::high_resolution_clock::now();
         gpu_bfs_result = cuda_bfs(csr_graph, graph.bfs_src, &elapsedTime);
-        end = clock();
-        double gpu_bfs_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+        // end = clock();
+        // double gpu_bfs_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+        end = std::chrono::high_resolution_clock::now();
+        double gpu_bfs_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
 
-        // // elapsedTime = 0;
-        // cuda_bfs(csr_graph, graph.bfs_src, &elapsedTime);
-        // double gpu_bfs_time;
-
-        printf("GPU BFS cost time: %f ms\n", gpu_bfs_time);
-        elapsedTime = 0;
+        printf("GPU BFS cost time: %f s\n", gpu_bfs_time);
 
         bfs_checker(graph, cpu_bfs_result, gpu_bfs_result, bfs_pass);
         string bfs_pass_label = "No";
@@ -95,19 +102,20 @@ int main()
     if (graph.sup_wcc) {
         int wcc_pass = 0;
         std::vector<std::vector<int>> cpu_wcc_result;
-        start = clock();
+
+        begin = std::chrono::high_resolution_clock::now();
         cpu_wcc_result = CPU_connected_components<double>(graph.OUTs);
-        end = clock();
-        double cpu_wcc_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        printf("CPU WCC cost time: %f ms\n", cpu_wcc_time);
+        end = std::chrono::high_resolution_clock::now();
+        double cpu_wcc_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("CPU WCC cost time: %f s\n", cpu_wcc_time);
 
         std::vector<std::vector<int>> gpu_wcc_result;
         elapsedTime = 0;
-        start = clock();
+        begin = std::chrono::high_resolution_clock::now();
         gpu_wcc_result = gpu_connected_components(csr_graph, &elapsedTime);
-        end = clock();
-        double gpu_wcc_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        printf("GPU WCC cost time: %f ms\n", gpu_wcc_time);
+        end = std::chrono::high_resolution_clock::now();
+        double gpu_wcc_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("GPU WCC cost time: %f s\n", gpu_wcc_time);
 
         wcc_checker(graph, cpu_wcc_result, gpu_wcc_result, wcc_pass);
         string wcc_pass_label = "No";
@@ -121,19 +129,19 @@ int main()
 
     if (graph.sup_sssp) {
         int sssp_pass = 0;
-        start = clock();
+        begin = std::chrono::high_resolution_clock::now();
         std::vector<double> cpu_sssp_result = CPU_shortest_paths(graph.OUTs, graph.sssp_src);
-        end = clock();
-        double cpu_sssp_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        printf("CPU SSSP cost time: %f ms\n", cpu_sssp_time);
+        end = std::chrono::high_resolution_clock::now();
+        double cpu_sssp_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("CPU SSSP cost time: %f s\n", cpu_sssp_time);
     
         elapsedTime = 0;
         std::vector<double> gpu_sssp_result(graph.V, 0);
-        start = clock();
+        begin = std::chrono::high_resolution_clock::now();
         gpu_shortest_paths(csr_graph, graph.sssp_src, gpu_sssp_result, &elapsedTime);
-        end = clock();
-        double gpu_sssp_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        printf("GPU SSSP cost time: %f ms\n", gpu_sssp_time);
+        end = std::chrono::high_resolution_clock::now();
+        double gpu_sssp_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("GPU SSSP cost time: %f s\n", gpu_sssp_time);
 
         sssp_checker(graph, cpu_sssp_result, gpu_sssp_result, sssp_pass);
 
@@ -146,23 +154,22 @@ int main()
     }
 
     if (graph.sup_pr) {
-        int pr_pass = 0;
-        vector<double> cpu_pr_result, gpu_pr_result;
-        start = clock();
-        // CPU_PageRank(graph, cpu_pr_result);
-        //update2
-        cpu_pr_result = PageRank(graph.INs, graph.OUTs, graph.pr_damping, graph.pr_its);
-        end = clock();
-        double cpu_pr_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        // pr_checker_cpu(graph, cpu_pr_result, pr_pass);
-        printf("CPU PageRank cost time: %f ms\n", cpu_pr_time);
 
+        vector<double> cpu_pr_result, gpu_pr_result;
+        int pr_pass = 0;
+        begin = std::chrono::high_resolution_clock::now();
+        cpu_pr_result = PageRank(graph.INs, graph.OUTs, graph.pr_damping, graph.pr_its);
+        end = std::chrono::high_resolution_clock::now();
+        double cpu_pr_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        
+        printf("CPU PageRank cost time: %f s\n", cpu_pr_time);
+        
         elapsedTime = 0;
-        start = clock();
+        begin = std::chrono::high_resolution_clock::now();
         gpu_PageRank(graph, &elapsedTime, gpu_pr_result);
-        end = clock();
-        double gpu_pr_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        printf("GPU PageRank cost time: %f ms\n", gpu_pr_time);
+        end = std::chrono::high_resolution_clock::now();
+        double gpu_pr_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("GPU PageRank cost time: %f s\n", gpu_pr_time);
 
         pr_checker(graph, cpu_pr_result, gpu_pr_result, pr_pass);
 
@@ -177,29 +184,22 @@ int main()
     if (graph.sup_cdlp) {
         int cdlp_pass = 0;
         std::vector<int> ans_cpu, ans_gpu;
-        start = clock();
+
+        begin = std::chrono::high_resolution_clock::now();
         CPU_Community_Detection(graph, ans_cpu);
-        end = clock();
-        double cpu_cdlp_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        // cdlp_check_cpu(graph, ans_cpu, cdlp_pass);
-        printf("CPU Community Detection cost time: %f ms\n", cpu_cdlp_time);
-
-        // start = clock();
-        // vector<int> cdlp_result = community_detection_CDP(graph, graph.cdlp_max_its);
-        // end = clock();
-        // double time_cpu_cdlp = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        // printf("CPU_cdlp cost time: %f ms\n", time_cpu_cdlp);
-
-        // cdlp_check_cpu(graph, cdlp_result, cdlp_pass);
+        end = std::chrono::high_resolution_clock::now();
+        double cpu_cdlp_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("CPU Community Detection cost time: %f s\n", cpu_cdlp_time);
 
         elapsedTime = 0;
-        start = clock();
+        begin = std::chrono::high_resolution_clock::now();
         gpu_Community_Detection(graph, &elapsedTime, ans_gpu);
-        end = clock();
-        double gpu_cdlp_time = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-        printf("GPU Community Detection cost time: %f ms\n", gpu_cdlp_time);
-        // cdlp_check_gpu(graph, ans_gpu, cdlp_pass);
+        end = std::chrono::high_resolution_clock::now();
+        double gpu_cdlp_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() / 1e9; // s
+        printf("GPU Community Detection cost time: %f s\n", gpu_cdlp_time);
 
+        cdlp_check(graph, ans_cpu, ans_gpu, cdlp_pass);
+        
         string cdlp_pass_label = "No";
         if(cdlp_pass != 0) cdlp_pass_label = "Yes";
         umap_all_res.emplace("cpu_cdlp_time", std::to_string(cpu_cdlp_time));
