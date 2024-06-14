a1a6a437b1d3cc0ecd9556b9625a3b9470221872
CSLiuPeng <liu_peng_email@163.com>
2024-06-14 16:27:23 +0800

    add direct graph to wcc

--- Test-Framework-for-LDBC/include/CPU_connected_components.hpp
+++ Test-Framework-for-LDBC/include/CPU_connected_components.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 template<typename T> // T is float or double
-std::vector<std::vector<int>> CPU_connected_components(std::vector<std::vector<std::pair<int, T>>>& input_graph) {
+std::vector<std::vector<int>> CPU_connected_components(std::vector<std::vector<std::pair<int, T>>>& input_graph, std::vector<std::vector<std::pair<int, T>>>& output_graph) {
 	//Using BFS method to find connectivity vectors starting from each node
 	/*this is to find connected_components using breadth first search; time complexity O(|V|+|E|);
 	related content: https://www.boost.org/doc/libs/1_68_0/boost/graph/connected_components.hpp
@@ -39,6 +39,16 @@ std::vector<std::vector<int>> CPU_connected_components(std::vector<std::vector<s
 						Q.push(adj_v);
 						component.push_back(adj_v);
 						discovered[adj_v] = true;
+					}
+				}
+
+				int adk_size = output_graph[v].size();
+				for(int k = 0; k < adk_size; ++k){
+					int adk_v = output_graph[v][k].first;
+					if(discovered[adk_v] == false){
+						Q.push(adk_v);
+						component.push_back(adk_v);
+						discovered[adk_v] = true;
 					}
 				}
 			}
