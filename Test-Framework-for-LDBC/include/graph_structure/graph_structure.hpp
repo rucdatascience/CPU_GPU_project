0f221069c8044bae756ec815318d51c77f2f1887
rucdatascience <87228246+rucdatascience@users.noreply.github.com>
2024-06-21 15:21:18 +0800

    1

--- Test-Framework-for-LDBC/include/graph_structure/graph_structure.hpp
+++ Test-Framework-for-LDBC/include/graph_structure/graph_structure.hpp
@@ -81,7 +81,6 @@ public:
 	inline void clear();// clear graph
 	inline int out_degree(int);//get graph out degree
 	inline int in_degree(int);//get graph in degree
-	inline CSR_graph<weight_type> toCSR();//use csr data format to store graph
 
 	/* 
 	LDBC test process
@@ -131,19 +130,7 @@ public:
 	
 };
 
-/*for GPU*/
 
-template <typename weight_type>
-class CSR_graph {
-	public:
-		std::vector<int> INs_Neighbor_start_pointers, OUTs_Neighbor_start_pointers; // Neighbor_start_pointers[i] is the start point of neighbor information of vertex i in Edges and Edge_weights
-		/*
-			Now, Neighbor_sizes[i] = Neighbor_start_pointers[i + 1] - Neighbor_start_pointers[i].
-			And Neighbor_start_pointers[V] = Edges.size() = Edge_weights.size() = the total number of edges.
-		*/
-		std::vector<int> INs_Edges, OUTs_Edges;  // Edges[Neighbor_start_pointers[i]] is the start of Neighbor_sizes[i] neighbor IDs
-		std::vector<weight_type> INs_Edge_weights, OUTs_Edge_weights; // Edge_weights[Neighbor_start_pointers[i]] is the start of Neighbor_sizes[i] edge weights
-};
 
 /*class member functions*/
 
@@ -270,42 +257,6 @@ void graph_structure<weight_type>::print() {
 	}
 	std::cout << "graph_structure_print END" << std::endl;
 
-}
-
-/*for GPU*/
-
-template <typename weight_type>
-CSR_graph<weight_type> graph_structure<weight_type>::toCSR() {
-
-	CSR_graph<weight_type> ARRAY;
-
-	int V = OUTs.size();
-	ARRAY.INs_Neighbor_start_pointers.resize(V + 1); // Neighbor_start_pointers[V] = Edges.size() = Edge_weights.size() = the total number of edges.
-	ARRAY.OUTs_Neighbor_start_pointers.resize(V + 1);
-
-	int pointer = 0;
-	for (int i = 0; i < V; i++) {
-		ARRAY.INs_Neighbor_start_pointers[i] = pointer;
-		for (auto& xx : INs[i]) {
-			ARRAY.INs_Edges.push_back(xx.first);
-			ARRAY.INs_Edge_weights.push_back(xx.second);
-		}
-		pointer += INs[i].size();
-	}
-	ARRAY.INs_Neighbor_start_pointers[V] = pointer;
-
-	pointer = 0;
-	for (int i = 0; i < V; i++) {
-		ARRAY.OUTs_Neighbor_start_pointers[i] = pointer;
-		for (auto& xx : OUTs[i]) {
-			ARRAY.OUTs_Edges.push_back(xx.first);
-			ARRAY.OUTs_Edge_weights.push_back(xx.second);
-		}
-		pointer += OUTs[i].size();
-	}
-	ARRAY.OUTs_Neighbor_start_pointers[V] = pointer;
-
-	return ARRAY;
 }
 
 template <typename weight_type>
