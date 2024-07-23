65c2a01085413aa064443831a0f2f4e1474205db
rucdatascience <87228246+rucdatascience@users.noreply.github.com>
2024-07-23 21:47:37 +0800

    1

--- LDBC_test_python/neo4j_py/bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 循环三次
-for i in {1..3}
-do
-    # 运行Python脚本
-    python run_all.py
-done
--- gpu-test.sh
+++
@@ -1,7 +0,0 @@
-cd Test-Framework-for-LDBC/GPU_CSR
-rm -rf build
-mkdir build
-cd build
-cmake ..
-make
-./bin/Test
