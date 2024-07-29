ff79d575da692071e1fe089100705870c4f117fe
JongMelon <2689526133@qq.com>
2024-07-29 17:01:57 +0800

    Update .sh files

---
+++ LDBC-CPU.sh
@@ -0,0 +1,21 @@
+data_dir="/home/mabojing/data"
+
+executable="/home/zhukai/CPU_GPU_project/build/bin_cpu/Test_CPU"
+
+filenames=("datagen-7_5-fb" "datagen-7_8-zf" "datagen-7_6-fb" "dota-league" "datagen-7_9-fb")
+
+for filename in "${filenames[@]}"
+do
+    echo "testing file $filename ..."
+    echo "$data_dir/$filename.properties" > input.txt
+    echo "$data_dir/$filename.v" >> input.txt
+    echo "$data_dir/$filename.e" >> input.txt
+    echo "$data_dir/$filename-BFS" >> input.txt
+    echo "$data_dir/$filename-SSSP" >> input.txt
+    echo "$data_dir/$filename-WCC" >> input.txt
+    echo "$data_dir/$filename-PR" >> input.txt
+    echo "$data_dir/$filename-CDLP" >> input.txt
+
+    $executable < input.txt > "/home/zhukai/CPU_GPU_project/results/$filename-CPU.output"
+    echo "test file $filename done."
+done
