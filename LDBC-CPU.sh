87f398c6d8d562850f63cb04e69c9cc5c093a46a
JongMelon <2689526133@qq.com>
2024-08-02 00:01:41 +0800

    Update LDBC-CPU.sh and LDBC-GPU.sh

--- LDBC-CPU.sh
+++ LDBC-CPU.sh
@@ -1,21 +1,17 @@
-data_dir="/home/mabojing/data"
+# run chmod +x ./LDBC-CPU.sh to make this file executable
 
-executable="/home/zhukai/CPU_GPU_project/build/bin_cpu/Test_CPU"
+data_dir="/home/jongmelon/LDBC-data"
 
-filenames=("datagen-7_5-fb" "datagen-7_8-zf" "datagen-7_6-fb" "dota-league" "datagen-7_9-fb")
+executable="/home/jongmelon/CPU_GPU_project/build/bin_cpu/Test_CPU"
+
+filenames=("datagen-7_5-fb")
 
 for filename in "${filenames[@]}"
 do
     echo "testing file $filename ..."
-    echo "$data_dir/$filename.properties" > input.txt
-    echo "$data_dir/$filename.v" >> input.txt
-    echo "$data_dir/$filename.e" >> input.txt
-    echo "$data_dir/$filename-BFS" >> input.txt
-    echo "$data_dir/$filename-SSSP" >> input.txt
-    echo "$data_dir/$filename-WCC" >> input.txt
-    echo "$data_dir/$filename-PR" >> input.txt
-    echo "$data_dir/$filename-CDLP" >> input.txt
+    echo "$data_dir/" > input.txt
+    echo "$filename" >> input.txt
 
-    $executable < input.txt > "/home/zhukai/CPU_GPU_project/results/$filename-CPU.output"
+    $executable < input.txt > "/home/jongmelon/CPU_GPU_project/logs/$filename-CPU.output"
     echo "test file $filename done."
 done
--- LDBC-GPU.sh
+++ LDBC-GPU.sh
@@ -1,22 +1,17 @@
-data_dir="/home/mabojing/data"
+# run chmod +x ./LDBC-GPU.sh to make this file executable
 
-executable="/home/zhukai/CPU_GPU_project/build/bin_gpu/Test_GPU"
+data_dir="/home/jongmelon/LDBC-data"
 
-#filenames=("datagen-7_5-fb" "datagen-7_8-zf" "datagen-7_6-fb" "dota-league" "datagen-7_9-fb")
-filenames=("kgs")
+executable="/home/jongmelon/CPU_GPU_project/build/bin_gpu/Test_GPU"
+
+filenames=("datagen-7_5-fb")
 
 for filename in "${filenames[@]}"
 do
     echo "testing file $filename ..."
-    echo "$data_dir/$filename.properties" > input.txt
-    echo "$data_dir/$filename.v" >> input.txt
-    echo "$data_dir/$filename.e" >> input.txt
-    echo "$data_dir/$filename-BFS" >> input.txt
-    echo "$data_dir/$filename-SSSP" >> input.txt
-    echo "$data_dir/$filename-WCC" >> input.txt
-    echo "$data_dir/$filename-PR" >> input.txt
-    echo "$data_dir/$filename-CDLP" >> input.txt
+    echo "$data_dir/" > input.txt
+    echo "$filename" >> input.txt
 
-    $executable < input.txt > "/home/zhukai/CPU_GPU_project/results/$filename-GPU.output"
+    $executable < input.txt > "/home/jongmelon/CPU_GPU_project/logs/$filename-GPU.output"
     echo "test file $filename done."
 done
