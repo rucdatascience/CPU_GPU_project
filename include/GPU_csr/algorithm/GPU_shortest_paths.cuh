f827c939ebc7e28154e70dd74721ac4f069bfdbf
JongMelon <2689526133@qq.com>
2024-08-14 18:21:14 +0800

    commented

--- include/GPU_csr/algorithm/GPU_shortest_paths.cuh
+++ include/GPU_csr/algorithm/GPU_shortest_paths.cuh
@@ -18,6 +18,7 @@ void gpu_sssp_pre(CSR_graph<double>& input_graph, int source, std::vector<double
 std::vector<std::pair<std::string, double>> Cuda_SSSP(graph_structure<double>& graph, CSR_graph<double>& csr_graph, std::string src_v, double max_dis = 10000000000);
 std::vector<std::pair<std::string, double>> Cuda_SSSP_pre(graph_structure<double>& graph, CSR_graph<double>& csr_graph, std::string src_v, std::vector<int>& pre_v, double max_dis = 10000000000);
 
+// this function is used to get the minimum value of double type atomically
 __device__ __forceinline__ double atomicMinDouble (double * addr, double value) {
     double old;
     old = __longlong_as_double(atomicMin((long long *)addr, __double_as_longlong(value)));
@@ -30,17 +31,20 @@ __global__ void Relax(int* out_pointer, int* out_edge, double* out_edge_weight, 
     if (idx < *queue_size) {
         int v = queue[idx];
 
+        // for all adjacent vertices
         for (int i = out_pointer[v]; i < out_pointer[v + 1]; i++) {
             int new_v = out_edge[i];
             double weight = out_edge_weight[i];
 
             double new_w = dis[v] + weight;
 
+            // try doing relaxation
             double old = atomicMinDouble(&dis[new_v], new_w);
 
             if (old <= new_w)
 				continue;
 
+            // if the distance is updated, set the vertex as visited
             atomicExch(&visited[new_v], 1);
         }
     }
@@ -58,12 +62,16 @@ __global__ void Relax_pre(int* out_pointer, int* out_edge, double* out_edge_weig
 
             double new_w = dis[v] + weight;
 
+            // try doing relaxation
+            // mutex is used
             while (atomicCAS(&mutex[new_v], 0, 1) != 0);
 
             if (new_w < dis[new_v]) {
                 dis[new_v] = new_w;
+                // update the previous vertex
                 pre[new_v] = v;
                 atomicExch(&visited[new_v], 1);
+                // share the updated distance with other threads in different blocks
                 __threadfence();
             }
 
@@ -73,10 +81,12 @@ __global__ void Relax_pre(int* out_pointer, int* out_edge, double* out_edge_weig
 }
 
 __global__ void CompactQueue(int V, int* next_queue, int* next_queue_size, int* visited) {
+    // this function is used to ensure that each necessary vertex is only pushed into the queue once
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < V && visited[idx]) {
         int pos = atomicAdd(next_queue_size, 1);
         next_queue[pos] = idx;
+        // reset the visited flag
         visited[idx] = 0;
     }
 }
@@ -94,6 +104,7 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
     int* queue, * next_queue;
     int* queue_size, * next_queue_size;
 
+    // allocate memory on GPU
     cudaMallocManaged((void**)&dis, V * sizeof(double));
     cudaMallocManaged((void**)&visited, V * sizeof(int));
     cudaMallocManaged((void**)&queue, V * sizeof(int));
@@ -101,6 +112,7 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
     cudaMallocManaged((void**)&queue_size, sizeof(int));
     cudaMallocManaged((void**)&next_queue_size, sizeof(int));
 
+    // synchronize the device
     cudaDeviceSynchronize();
     cudaError_t cuda_status = cudaGetLastError();
     if (cuda_status != cudaSuccess) {
@@ -109,6 +121,7 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
     }
 
     for (int i = 0; i < V; i++) {
+        // initialize the distance array and visited array
 		dis[i] = max_dis;
 		visited[i] = 0;
 	}
@@ -124,6 +137,7 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
 
     while (*queue_size > 0) {
 		numBlocks = (*queue_size + threadsPerBlock - 1) / threadsPerBlock;
+        // launch the kernel function to relax the edges
 		Relax <<< numBlocks, threadsPerBlock >>> (out_pointer, out_edge, out_edge_weight, dis, queue, queue_size, visited);
 		cudaDeviceSynchronize();
 
@@ -134,6 +148,7 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
         }
 
 		numBlocks = (V + threadsPerBlock - 1) / threadsPerBlock;
+        // do the compact operation
 		CompactQueue <<< numBlocks, threadsPerBlock >>> (V, next_queue, next_queue_size, visited);
 		cudaDeviceSynchronize();
 
@@ -142,7 +157,8 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
 			fprintf(stderr, "CompactQueue kernel launch failed: %s\n", cudaGetErrorString(cuda_status));
 			return;
 		}
-		
+
+        // swap the queue and next_queue
         std::swap(queue, next_queue);
 
 		*queue_size = *next_queue_size;
@@ -151,6 +167,7 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
 
     cudaMemcpy(distance.data(), dis, V * sizeof(double), cudaMemcpyDeviceToHost);
 
+    // free the memory
     cudaFree(dis);
     cudaFree(visited);
     cudaFree(queue);
@@ -162,6 +179,7 @@ void gpu_shortest_paths(CSR_graph<double>& input_graph, int source, std::vector<
 }
 
 void gpu_sssp_pre(CSR_graph<double>& input_graph, int source, std::vector<double>& distance, std::vector<int>& pre_v, double max_dis) {
+    // the only difference between this function and the previous one is that we need to record the previous vertex
     int V = input_graph.OUTs_Neighbor_start_pointers.size() - 1;
     int E = input_graph.OUTs_Edges.size();
 
@@ -256,6 +274,7 @@ std::vector<std::pair<std::string, double>> Cuda_SSSP(graph_structure<double>& g
     std::vector<double> gpuSSSPvec(graph.V, 0);
     gpu_shortest_paths(csr_graph, src_v_id, gpuSSSPvec, max_dis);
 
+    // transfer the vertex id to vertex name
     return graph.res_trans_id_val(gpuSSSPvec);
 }
 
@@ -265,6 +284,7 @@ std::vector<std::pair<std::string, double>> Cuda_SSSP_pre(graph_structure<double
     pre_v.resize(graph.V);
     gpu_sssp_pre(csr_graph, src_v_id, gpuSSSPvec, pre_v, max_dis);
 
+    // check the correctness of the previous vertex
     for (int i = 0; i < graph.V; i++) {
 		double dis = gpuSSSPvec[i];
 		int pre = pre_v[i];
