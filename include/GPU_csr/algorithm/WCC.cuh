bde1500af161d1e2a5a7fa32c6c7ee5167fd0e12
JongMelon <2689526133@qq.com>
2024-08-20 23:36:26 +0800

    debug

--- include/GPU_csr/algorithm/WCC.cuh
+++ include/GPU_csr/algorithm/WCC.cuh
@@ -5,6 +5,7 @@
 #include "device_launch_parameters.h"
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
+#include <thrust/extrema.h>
 #include <vector>
 #include <string.h>
 #include <GPU_csr/GPU_csr.hpp>
@@ -37,9 +38,7 @@ __global__ void get_freq(int *parent, int *freq, int N)
     int tid = blockIdx.x * blockDim.x + threadIdx.x; // tid decides process which vertex
 
     if (tid < N) // tid decides process which vertex
-    {
-        atomicAdd(freq + parent[tid], 1);
-    }
+        atomicAdd(&freq[parent[tid]], 1);
 }
 
 
@@ -60,7 +59,7 @@ __global__ void sampling(int *all_pointer, int *all_edge, int *parent, int N, in
             { // link
                 int h = p1 > p2 ? p1 : p2;
                 int l = p2 >= p1 ? p1 : p2;
-                int check = atomicCAS(parent + h, h, l);
+                int check = atomicCAS(&parent[h], h, l);
                 if (check == h)
                 {
                     break;
@@ -91,7 +90,7 @@ __global__ void full_link(int *all_pointer, int *all_edge, int *parent, int most
             { // link
                 int h = p1 > p2 ? p1 : p2;
                 int l = p2 >= p1 ? p1 : p2;
-                int check = atomicCAS(parent + h, h, l);
+                int check = atomicCAS(&parent[h], h, l);
                 if (check == h)
                 {
                     break;
@@ -104,18 +103,18 @@ __global__ void full_link(int *all_pointer, int *all_edge, int *parent, int most
 }
 std::vector<int> WCC_GPU(graph_structure<double> &graph, CSR_graph<double> &input_graph)
 {
-    int N = graph.size();
-    long long E = input_graph.E_all;                                                      // number of edges in the graph                                                            // number of vertices in the graph
+    int N = graph.size(); // number of vertices in the graph
+
     dim3 init_label_block((N + WCCG_THREAD_PER_BLOCK - 1) / WCCG_THREAD_PER_BLOCK, 1, 1); // the number of blocks used in the gpu
     dim3 init_label_thread(WCCG_THREAD_PER_BLOCK, 1, 1);                                  // the number of threads used in the gpu
 
-    int *all_edge = input_graph.all_edge; // graph stored in csr format
-    int *all_pointer = input_graph.all_pointer;
+    int *all_edge = input_graph.in_edge; // graph stored in csr format
+    int *all_pointer = input_graph.in_pointer;
     int *parent = nullptr;
     int *freq = nullptr;
     cudaMallocManaged((void **)&parent, N * sizeof(int));
     cudaMallocManaged((void **)&freq, N * sizeof(int));
-    cudaMemset(freq,0,N * sizeof(int));
+    cudaMemset(freq, 0, N * sizeof(int));
     parent_init<<<init_label_block, init_label_thread>>>(parent, N);
     cudaDeviceSynchronize();
     int it = 0, ITERATION = 2; // number of iterations
@@ -127,8 +126,8 @@ std::vector<int> WCC_GPU(graph_structure<double> &graph, CSR_graph<double> &inpu
         cudaDeviceSynchronize();
         it++;
     }
-    get_freq<<<init_label_block, init_label_thread>>>(parent, freq,N);
-    int *c = thrust::max_element(freq, freq + N);
+    get_freq<<<init_label_block, init_label_thread>>>(parent, freq, N);
+    int *c = thrust::max_element(thrust::device, freq, freq + N);
     int most_f_element = *c;
     full_link<<<init_label_block, init_label_thread>>>(all_pointer, all_edge, parent, most_f_element, N, ITERATION);
     cudaDeviceSynchronize();
@@ -137,7 +136,6 @@ std::vector<int> WCC_GPU(graph_structure<double> &graph, CSR_graph<double> &inpu
     
     std::vector<int> result(N);
     cudaMemcpy(result.data(), parent, N * sizeof(int), cudaMemcpyDeviceToHost);
-    cout<<endl;
     cudaFree(parent);
     cudaFree(freq);
     return result;
