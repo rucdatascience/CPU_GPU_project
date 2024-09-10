fdc35d42b615d24dc504cf98d3d39bf08b1778bb
JongMelon <2689526133@qq.com>
2024-09-10 19:09:03 +0800

    Update CSR for undirected graph

--- include/GPU_csr/GPU_csr.hpp
+++ include/GPU_csr/GPU_csr.hpp
@@ -11,6 +11,9 @@ class CSR_graph
 public:
     CSR_graph() {}
     ~CSR_graph();
+
+    bool is_directed = true; // direct graph or undirect graph
+
     std::vector<int> INs_Neighbor_start_pointers, OUTs_Neighbor_start_pointers, ALL_start_pointers; // Neighbor_start_pointers[i] is the start point of neighbor information of vertex i in Edges and Edge_weights
     /*
         Now, Neighbor_sizes[i] = Neighbor_start_pointers[i + 1] - Neighbor_start_pointers[i].
@@ -25,28 +28,32 @@ public:
 
 template <typename weight_type>
 // CSR_graph<weight_type> toCSR(graph_structure<weight_type>& graph)
-CSR_graph<weight_type> toCSR(graph_structure<weight_type>& graph)
+CSR_graph<weight_type> toCSR(graph_structure<weight_type>& graph, bool is_directed = true)
 {
 
     CSR_graph<weight_type> ARRAY;
 
+    ARRAY.is_directed = is_directed;
+
     int V = graph.size();
-    ARRAY.INs_Neighbor_start_pointers.resize(V + 1); // Neighbor_start_pointers[V] = Edges.size() = Edge_weights.size() = the total number of edges.
     ARRAY.OUTs_Neighbor_start_pointers.resize(V + 1);
-    ARRAY.ALL_start_pointers.resize(V + 1);
-    
+
     int pointer = 0;
-    for (int i = 0; i < V; i++)
-    {
-        ARRAY.INs_Neighbor_start_pointers[i] = pointer;
-        for (auto &xx : graph.INs[i])
+
+    if (is_directed) {
+        ARRAY.INs_Neighbor_start_pointers.resize(V + 1); // Neighbor_start_pointers[V] = Edges.size() = Edge_weights.size() = the total number of edges.
+        for (int i = 0; i < V; i++)
         {
-            ARRAY.INs_Edges.push_back(xx.first);
-            ARRAY.INs_Edge_weights.push_back(xx.second);
+            ARRAY.INs_Neighbor_start_pointers[i] = pointer;
+            for (auto &xx : graph.INs[i])
+            {
+                ARRAY.INs_Edges.push_back(xx.first);
+                ARRAY.INs_Edge_weights.push_back(xx.second);
+            }
+            pointer += graph.INs[i].size();
         }
-        pointer += graph.INs[i].size();
+        ARRAY.INs_Neighbor_start_pointers[V] = pointer;
     }
-    ARRAY.INs_Neighbor_start_pointers[V] = pointer;
 
     pointer = 0;
     for (int i = 0; i < V; i++)
@@ -61,49 +68,71 @@ CSR_graph<weight_type> toCSR(graph_structure<weight_type>& graph)
     }
     ARRAY.OUTs_Neighbor_start_pointers[V] = pointer;
 
-    pointer = 0;
-    for (int i = 0; i < V; i++)
-    {
-        ARRAY.ALL_start_pointers[i] = pointer;
-        for (auto &xx : graph.INs[i])
+    if (is_directed) {
+        ARRAY.ALL_start_pointers.resize(V + 1);
+        pointer = 0;
+        for (int i = 0; i < V; i++)
         {
-            ARRAY.all_Edges.push_back(xx.first);
+            ARRAY.ALL_start_pointers[i] = pointer;
+            for (auto &xx : graph.INs[i])
+            {
+                ARRAY.all_Edges.push_back(xx.first);
+            }
+            for (auto &xx : graph.OUTs[i])
+            {
+                ARRAY.all_Edges.push_back(xx.first);
+            }
+            pointer += graph.INs[i].size() + graph.OUTs[i].size();
         }
-        for (auto &xx : graph.OUTs[i])
-        {
-            ARRAY.all_Edges.push_back(xx.first);
-        }
-        pointer += graph.INs[i].size() + graph.OUTs[i].size();
+        ARRAY.ALL_start_pointers[V] = pointer;
     }
-    ARRAY.ALL_start_pointers[V] = pointer;
 
-    size_t E_in = ARRAY.INs_Edges.size();
+    size_t E_in = 0;
     size_t E_out = ARRAY.OUTs_Edges.size();
-    size_t E_all = E_in+E_out;
+    size_t E_all = E_out;
+    
     ARRAY.E_all = E_all;
-    cudaMallocManaged((void**)&ARRAY.in_pointer, (V + 1) * sizeof(int));
+
     cudaMallocManaged((void**)&ARRAY.out_pointer, (V + 1) * sizeof(int));
-    cudaMallocManaged((void**)&ARRAY.all_pointer, (V + 1) * sizeof(int));
-    cudaMallocManaged((void**)&ARRAY.in_edge, E_in * sizeof(int));
     cudaMallocManaged((void**)&ARRAY.out_edge, E_out * sizeof(int));
-    cudaMallocManaged((void**)&ARRAY.all_edge, E_all * sizeof(int));
-    cudaMallocManaged((void**)&ARRAY.in_edge_weight, E_in * sizeof(double));
     cudaMallocManaged((void**)&ARRAY.out_edge_weight, E_out * sizeof(double));
+
+    if (is_directed) {
+        E_in = ARRAY.INs_Edges.size();
+        E_all = E_in + E_out;
+        ARRAY.E_all = E_all;
+
+        cudaMallocManaged((void**)&ARRAY.in_pointer, (V + 1) * sizeof(int));
+        cudaMallocManaged((void**)&ARRAY.all_pointer, (V + 1) * sizeof(int));
+        cudaMallocManaged((void**)&ARRAY.in_edge, E_in * sizeof(int));
+        cudaMallocManaged((void**)&ARRAY.all_edge, E_all * sizeof(int));
+        cudaMallocManaged((void**)&ARRAY.in_edge_weight, E_in * sizeof(double));
+    }
+    else {
+        ARRAY.in_pointer = ARRAY.out_pointer;
+        ARRAY.all_pointer = ARRAY.out_pointer;
+        ARRAY.in_edge = ARRAY.out_edge;
+        ARRAY.all_edge = ARRAY.out_edge;
+        ARRAY.in_edge_weight = ARRAY.out_edge_weight;
+    }
 
     cudaDeviceSynchronize();
     cudaError_t error = cudaGetLastError();
     if (error != cudaSuccess) {
         printf("CUDA error: %s\n", cudaGetErrorString(error));
     }
-    
-    cudaMemcpy(ARRAY.in_pointer, ARRAY.INs_Neighbor_start_pointers.data(), (V + 1) * sizeof(int), cudaMemcpyHostToDevice);
+
     cudaMemcpy(ARRAY.out_pointer, ARRAY.OUTs_Neighbor_start_pointers.data(), (V + 1) * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(ARRAY.all_pointer, ARRAY.ALL_start_pointers.data(), (V + 1) * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(ARRAY.in_edge, ARRAY.INs_Edges.data(), E_in * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(ARRAY.out_edge, ARRAY.OUTs_Edges.data(), E_out * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(ARRAY.all_edge, ARRAY.all_Edges.data(), E_all * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(ARRAY.in_edge_weight, ARRAY.INs_Edge_weights.data(), E_in * sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(ARRAY.out_edge_weight, ARRAY.OUTs_Edge_weights.data(), E_out * sizeof(double), cudaMemcpyHostToDevice);
+
+    if (is_directed) {
+        cudaMemcpy(ARRAY.in_pointer, ARRAY.INs_Neighbor_start_pointers.data(), (V + 1) * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(ARRAY.all_pointer, ARRAY.ALL_start_pointers.data(), (V + 1) * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(ARRAY.in_edge, ARRAY.INs_Edges.data(), E_in * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(ARRAY.all_edge, ARRAY.all_Edges.data(), E_all * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(ARRAY.in_edge_weight, ARRAY.INs_Edge_weights.data(), E_in * sizeof(double), cudaMemcpyHostToDevice);
+    }
 
     return ARRAY;
 }
@@ -111,12 +140,16 @@ CSR_graph<weight_type> toCSR(graph_structure<weight_type>& graph)
 template <typename weight_type>
 CSR_graph<weight_type>::~CSR_graph()
 {
-    cudaFree(in_pointer);
     cudaFree(out_pointer);
-    cudaFree(all_pointer);
-    cudaFree(in_edge);
     cudaFree(out_edge);
-    cudaFree(all_edge);
-    cudaFree(in_edge_weight);
     cudaFree(out_edge_weight);
+
+    if (is_directed) {
+        cudaFree(in_pointer);
+        cudaFree(in_edge);
+        cudaFree(in_edge_weight);
+
+        cudaFree(all_pointer);
+        cudaFree(all_edge);
+    }
 }
